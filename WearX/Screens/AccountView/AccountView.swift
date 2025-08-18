d10d213924d01bc0ed0a1886d87ba0673d2af6dc
chertik77 <denysbabych.dev@gmail.com>
2025-08-18 15:22:16 +0100

    feat: add account view form

--- WearX/Screens/AccountView/AccountView.swift
+++ WearX/Screens/AccountView/AccountView.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var showOutOfStockItems = false
+    @State private var enableDarkMode = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    DatePicker(
+                        "Birthday",
+                        selection: $birthdate,
+                        displayedComponents: .date
+                    )
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save changes")
+                    }
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle(
+                        "Show Out of Stock Products",
+                        isOn: $showOutOfStockItems
+                    )
+                    Toggle(
+                        "Enable Dark Mode",
+                        isOn: $enableDarkMode
+                    )
+                } header: {
+                    Text("Requests")
+                }
+            }
+            .navigationTitle("Account")
+            .tint(.orange)
         }
     }
 }
