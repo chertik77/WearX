a846713b79d7eecf89421a446598b72c03c918b7
chertik77 <denysbabych.dev@gmail.com>
2025-08-18 11:15:02 +0100

    Initial Commit

---
+++ WearX/WearXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WearXApp.swift
+//  WearX
+//
+//  Created by Denys Babych on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct WearXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
